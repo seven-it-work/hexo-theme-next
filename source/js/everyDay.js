2f04f3303e7f28506416b2398a51bf9e417c8848
y30016814 <yijialuo@huawei.com>
2024-09-10 15:08:59 +0800

    加入每日一句

---
+++ source/js/everyDay.js
@@ -0,0 +1,33 @@
+function getEveryDayStr() {
+  fetch('https://v.api.aa1.cn/api/yiyan/index.php')
+    .then(function (res) {
+      return res.text()
+    }).then(everyDayStr => {
+    const s = everyDayStr.replace("<p>", "").replace("</p>", "");
+    setTimeout(() => {
+      addEveryDayStr(s)
+    }, 1000)
+  }).catch(()=>{
+    setTimeout(() => {
+      addEveryDayStr("欢迎来到sevenyjl的博客")
+    }, 1000)
+  })
+}
+
+function addEveryDayStr(str) {
+  const text_info = document.getElementById("every-dat-text")
+  const str_split = str.split("")
+  const p = document.createElement("span");
+  p.classList.add('every-dat-animation');
+  p.innerText = "";
+  text_info.appendChild(p);
+//遍历设置为span，定时span的属性中加入动画，
+  for (let i = 0; i < str_split.length; i++) {
+    setTimeout(() => {
+      const temp = document.createElement("span");
+      temp.classList.add('every-dat-animation');
+      temp.innerText = str_split[i];
+      text_info.appendChild(temp);
+    }, (i) * (Math.min(500-i,50)))
+  }
+}
